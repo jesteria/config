fcb03843001125bae416b19fe70892ec33e79a27
Jesse London <jesselondon@gmail.com>
2013-10-10 23:00:45 -0500

    improved git alias/command "compare"

--- git/gitcompare.bash
+++ git/gitcompare.bash
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+function commits() {
+    if [ -z "$2" ]
+    then
+        echo $1: none
+	return
+    fi
+
+    echo -e "========\n$1\n========\n"
+    printf "$2\n\n"
+}
+
 echo -e "compare $1\n"
 
 incoming=$(git log ..$1)
 outgoing=$(git log $1..)
 
-[ -z $incoming ] && incoming=none
-echo incoming: $incoming
-
-[ -z $outgoing ] && outgoing=none
-echo outgoing: $outgoing
+commits incoming "$incoming"
+[[ "$incoming" || "$outgoing" ]] && echo ""
+commits outgoing "$outgoing"
